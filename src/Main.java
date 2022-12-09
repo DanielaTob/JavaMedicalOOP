b2b08c7772fd99555aaccd1ca5449db49525ef12
daniela <moreno.tobar.daniela@gmail.com>
2022-12-08 19:09:44 -0500

    First class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        Doctor myDoctor = new Doctor();
+        myDoctor.name = "Luke Skywalker";
+        myDoctor.showName();
     }
 }
