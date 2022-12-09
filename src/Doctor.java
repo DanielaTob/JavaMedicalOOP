2a36669c17774ccd575b695186369335fae983d4
daniela <moreno.tobar.daniela@gmail.com>
2022-12-08 19:03:09 -0500

    first commit

---
+++ src/Doctor.java
@@ -0,0 +1,12 @@
+public class Doctor {
+
+    //Atributos de la clase Doctor
+    int id;
+    String name;
+    String speciality;
+
+    //Comportamientos
+    public void showName(){
+        System.out.println(name);
+    }
+}
