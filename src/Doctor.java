9cdb7f84c061d2d6a917231ea0693518e90e61f4
daniela <moreno.tobar.daniela@gmail.com>
2022-12-09 18:05:30 -0500

    menu

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,12 +1,28 @@
 public class Doctor {
 
     //Atributos de la clase Doctor
-    int id;
+    static int id = 0; //Autoincrement
     String name;
     String speciality;
+
+    //Constructor
+    Doctor(){
+        System.out.println("Construyendo el Objeto Doctor");
+        id++;
+    }
+
+    Doctor(String name){
+        System.out.println("El nombre del doctor asignado es: " + name);
+    }
 
     //Comportamientos
     public void showName(){
         System.out.println(name);
+    }
+    public void showSpeciality(){
+        System.out.println(speciality);
+    }
+    public void showId(){
+        System.out.println("ID Doctor: " + id);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@ public class Main {
     public static void main(String[] args) {
         Doctor myDoctor = new Doctor();
         myDoctor.name = "Luke Skywalker";
+        myDoctor.speciality = "Psychology";
         myDoctor.showName();
+        myDoctor.showSpeciality();
+        myDoctor.showId();
+
+        Doctor.id++;
+
+        Doctor myDoctorLuke = new Doctor();
+        myDoctor.showId();
+
+        Menu.showMenu();
+
     }
 }
